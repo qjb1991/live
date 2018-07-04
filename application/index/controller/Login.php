4ef4d30769cf0cd62f4cc95a7db6c07007c48bd6
qjb1029 <qjb1029@126.com>
2018-07-04 18:10:11 +0800

    add

--- application/index/controller/Login.php
+++ application/index/controller/Login.php
@@ -14,14 +14,28 @@ use app\common\lib\redis\Predis;
 
 class Login{
     public function index(Request $request){
-        $phoneNum = $request->param('phone_num');
-        $code = $request->param('code');
-        if (empty($phoneNum) || empty($code)) {
-            return Util::show(config('code.val_empty'),'mobile or code is empty');
-        }
+        var_dump($request->param());
+        echo "<br>";
+        var_dump($request->get());
+        echo "<br>";
+        var_dump($request->input());
+        echo "<br>";
+        var_dump($_GET);
+//        $phoneNum = $request->param('phone_num');
+//        $code = $request->param('code');
+//        echo $phoneNum.PHP_EOL;
+//        echo $code.PHP_EOL;
+//        if (empty($phoneNum) || empty($code)) {
+//            return Util::show(config('code.val_empty'),'mobile or code is empty');
+//        }
+//
+//        //获取redis中的code
+//        try{
+//            $redisCode = Predis::getInstance()->get(Redis::smsKey($phoneNum));
+//        } catch (\Exception $e){
+//            echo $e->getMessage();
+//        }
+//        echo 'a'.$redisCode;
 
-        //获取redis中的code
-        $redisCode = Predis::getInstance()->get(Redis::smsKey($phoneNum));
-        echo $redisCode;
     }
 }
