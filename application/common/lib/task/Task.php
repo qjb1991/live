dab0552d429bce9b27992e81636edbdfc1914a9b
qjb1029 <qjb1029@126.com>
2018-07-10 18:12:45 +0800

    0710

--- application/common/lib/task/Task.php
+++ application/common/lib/task/Task.php
@@ -4,14 +4,21 @@
  */
 namespace app\common\lib\task;
 use app\common\lib\ali\Sms;
+use app\common\lib\Redis;
+use app\common\lib\redis\Predis;
 
 class Task{
     public function sendSms($data){
         try{
             $response = Sms::sendSms($data['mobile'], $data['code']);
         } catch (\Exception $e){
-            echo $e->getMessage();
+            return false;
         }
-        print_r($response);
+
+        //发送成功则记录到redis里面
+        if ($response->Code === 'OK'){
+            Predis::getInstance()->set(Redis::smsKey($data['mobile']), $data['code'], 120);
+        }
+        return true;
     }
 }
---
+++ live.sql
@@ -0,0 +1,5 @@
+//球队表
+create table `live_team`(
+  `id` tinyint(1) unsigned not null auto_increment,
+  `name`
+)
