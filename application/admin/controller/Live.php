03184a8f3e2e1b74d011cc893dc3f8f9278c7781
qjb1991 <375512114@qq.com>
2018-07-18 00:12:54 +0800

    a

--- application/admin/controller/Live.php
+++ application/admin/controller/Live.php
@@ -10,8 +10,21 @@ use app\common\lib\Util;
 
 class Live{
     public function push(){
-        print_r($_GET);
+        if(empty($_GET)){
+            return Util::show(config('code.error'), 'error');
+        }
+        $teams = [
+            1   =>  [
+                'name'  =>  '马刺',
+                'logo'  =>  '/live/imgs/team1.png',
+            ],
+            4   =>  [
+                'name'  =>  '火箭',
+                'logo'  =>  '/live/imgs/team2.png',
+            ],
+        ];
 
+        $data = 
         $_POST['http_server']->push(8, 'hello-push');
     }
 }
